3e1dd08add9777a8a7649cd0343f9f904d7940b5
Omurkan819 <oabaskanov@mail.ru>
2021-05-12 20:25:32 +0600

    first commit in 89

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,4 @@
-console.log(1)
-console.log(2)
-setTimeout(() => {
-  console.log('callback function fired')
-}, 2000)
-console.log(3)
-console.log(4)
+const request = new XMLHttpRequest()
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+request.send()
