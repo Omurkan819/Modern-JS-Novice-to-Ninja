446bcc4a451882dd58970e45b934b0481c17cdfc
Omurkan819 <oabaskanov@mail.ru>
2021-05-13 01:23:43 +0600

    callbacks with err and data, in 91

--- sandbox.js
+++ sandbox.js
@@ -4,16 +4,17 @@ const getTodos = callback => {
   request.addEventListener('readystatechange', () => {
     // console.log(request, request.readyState)
     if (request.readyState === 4 && request.status === 200) {
-      callback()
+      callback(undefined, request.responseText)
     } else if (request.readyState === 4) {
-      callback()
+      callback('could not fetch data', undefined)
     }
   })
 
-  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+  request.open('GET', 'https://jsonplaceholder.typicode.com/todosd/')
   request.send()
 }
 
-getTodos(() => {
+getTodos((err, data) => {
   console.log('callback fired')
+  console.log(err, data)
 })
