a5b84575e5aa35f0dec45b631f072bcbf8bbe131
Omurkan819 <oabaskanov@mail.ru>
2021-05-13 01:15:03 +0600

    callback fired, in 91

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,19 @@
-const request = new XMLHttpRequest()
+const getTodos = callback => {
+  const request = new XMLHttpRequest()
 
-request.addEventListener('readystatechange', () => {
-  // console.log(request, request.readyState)
-  if (request.readyState === 4 && request.status === 200) {
-    console.log(request, request.responseText)
-  } else if (request.readyState === 4) {
-    console.log('could not fetch the data')
-  }
+  request.addEventListener('readystatechange', () => {
+    // console.log(request, request.readyState)
+    if (request.readyState === 4 && request.status === 200) {
+      callback()
+    } else if (request.readyState === 4) {
+      callback()
+    }
+  })
+
+  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+  request.send()
+}
+
+getTodos(() => {
+  console.log('callback fired')
 })
-
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
-request.send()
